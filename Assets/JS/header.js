6321481c6cf62917cba2f46dbe71ed216c57e41d
Railey Dela Peña <131561799+RaileySawada@users.noreply.github.com>
2024-04-23 19:30:51 +0800

    Delete Assets directory

--- Assets/JS/header.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger-menu");
-const backdrop = document.querySelector("nav ul");
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    backdrop.classList.toggle("active");
-});
